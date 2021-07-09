316ca59a9b9f7854eee0a8ffcadfa64a3e23f593
lunghekani <lunghivlanga@gmail.com>
2021-07-10 01:02:42 +0200

    Did basic and custom validation

---
+++ LearnMVC/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LearnMVC.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer) validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0||customer.MembershipTypeId == 1)
+            {
+                return ValidationResult.Success;
+            }
+
+            if (customer.Birthdate ==null)
+            {
+                return new ValidationResult("Birthdate is required.");
+            }
+
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+            return (age >= 18)
+                ? ValidationResult.Success
+                : new ValidationResult("In the words of Tyler, The Creator - You're too fucking young");
+        }
+    }
+}
