93afc74c0c6cf12fdf24ccaff74496b9c94bc24c
lunghekani <lunghivlanga@gmail.com>
2021-07-11 21:54:33 +0200

    Building the API

--- LearnMVC/Global.asax.cs
+++ LearnMVC/Global.asax.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace LearnMVC
 {
@@ -12,6 +14,7 @@ namespace LearnMVC
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
